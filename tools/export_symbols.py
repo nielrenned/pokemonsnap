4b5cb257c717fb9fa0827f33b4d3b111dfabe542
Jacob Dennerlein <nielrenned@gmail.com>
2026-07-25 23:30:12 -0400

    Fix symbol export

--- tools/export_symbols.py
+++ tools/export_symbols.py
@@ -66,7 +66,7 @@ def main():
         "apChecksum": (ap + 0x04, "u32", "r", "checksum over speciesScores"),
         "speciesScores": (ap + 0x08, "s16[73][6]", "r",
                           "[slot][0=special, 1=pose, 2=size, 3=technique, 4=samePkmn, 5=poseFlags]; slot=getSpeciesSlot(pokemonID, levelID)"),
-        "signsFound": (ap + 0x73 * 2 * 6, "u8[6]", "r", "[beach, tunnel, volcano, river, cave, valley]")
+        "signsFound": (ap + 0x08 + 73 * 2 * 6, "u8[6]", "r", "[beach, tunnel, volcano, river, cave, valley]")
     }
 
     # Identifiers: `rom_offset` is the z64 write offset, `addr` the RAM read addr.
